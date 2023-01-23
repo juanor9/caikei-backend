23e0b9fc11657670bbc1a6c0aa314fc6913d3e52
Juan Camilo Orjuela <orjuela9@gmail.com>
2023-01-23 14:21:37 -0500

    set checkhealth route

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,8 @@
 import { Application } from 'express';
+import healthcheck from './api/healthcheck';
 
 function routes(app: Application):void{
-
+    app.use('/api/healthcheck', healthcheck);
 }
   
 export default routes;
