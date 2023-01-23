d5bf8066a7e6c086f48cadfd3814ec5b630d848b
Juan Camilo Orjuela <orjuela9@gmail.com>
2023-01-23 13:28:28 -0500

    set structure for future routes

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import * as dotenv from 'dotenv';
 import express from 'express';
 import configExpress from './config/express';
 import configDb from './config/database';
+import routes from './routes';
 
 dotenv.config();
 const port = process.env.PORT || 8080;
@@ -11,6 +12,8 @@ const app = express();
 configExpress(app);
 // Set config file to Mongo
 configDb();
+// Set server routes
+routes(app);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
