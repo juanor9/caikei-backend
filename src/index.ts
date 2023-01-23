b57b0fc461647ca2f2ea936b813fb9c935e73fa3
Juan Camilo Orjuela <orjuela9@gmail.com>
2023-01-23 12:39:41 -0500

    config db

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import * as dotenv from 'dotenv';
 import express from 'express';
 import configExpress from './config/express';
+import configDb from './config/database';
 
 dotenv.config();
 const port = process.env.PORT || 8080;
@@ -8,6 +9,8 @@ const app = express();
 
 // Set config file to express
 configExpress(app);
+// Set config file to Mongo
+configDb();
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
