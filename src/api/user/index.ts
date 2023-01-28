1297698b0eab688b577789898d932e67770243b0
Juan Camilo Orjuela <orjuela9@gmail.com>
2023-01-28 17:04:44 -0500

    update services to update email and password

--- src/api/user/index.ts
+++ src/api/user/index.ts
@@ -2,13 +2,19 @@ import { Router } from "express";
 import {
     handleCreateUser,
     handleGetUser,
+    handleUpdateUser,
 } from "./user.controller";
+import { isAuthenticated } from "../../auth/auth.services";
 
 const router = Router();
 
-// POST api/user/
+// POST api/user/   ---Create user ---
 router.post("/", handleCreateUser);
 
-// GET api/user/
-router.get("/", handleGetUser);
+// GET api/user/    ---Get user ---
+router.get("/", isAuthenticated, handleGetUser);
+
+//PATCH api/user/ ---Update user ---
+router.patch("/:id", isAuthenticated, handleUpdateUser);
+
 export default router;
--- src/api/user/user.controller.ts
+++ src/api/user/user.controller.ts
@@ -1,7 +1,8 @@
 import { Request, Response } from "express";
 import crypto from "crypto";
-import { createUser } from "./user.services";
-import { verifyToken } from "../../auth/auth.services";
+import { createUser, updateUser } from "./user.services";
+import { verifyToken, signToken } from "../../auth/auth.services";
+import bcrypt from "bcryptjs";
 
 export async function handleCreateUser(req: Request, res: Response) {
   const data = req.body;
@@ -26,14 +27,44 @@ export async function handleGetUser(req: Request, res: Response) {
   try {
     const userToken = req.headers?.authorization?.split(" ")[1] as string;
     if (!userToken) {
-      return res
-        .status(401)
-        .json({
-          message: "You are not authorized to access to this information",
-        });
+      return res.status(401).json({
+        message: "You are not authorized to access to this information",
+      });
     }
     const decoded = verifyToken(userToken);
     return res.status(200).json(decoded);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+}
+
+export async function handleUpdateUser(req: Request, res: Response) {
+  console.log("in handleUpdateUser");
+  const { id } = req.params;
+  const data = req.body;
+  try {
+    // verify if new data is password
+    if (data.password) {
+      // encrypt password
+      const salt = await bcrypt.genSalt(10);
+      const hash = await bcrypt.hash(data.password, salt);
+      data.password = hash;
+    }
+    const user = await updateUser(id, data);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found to update" });
+    }
+
+    //create a new token with new profile
+    const newToken = signToken(user.profile);
+    //create object with new data to pass to update
+    const newData = {
+      newUser: user,
+      newToken: newToken,
+    };
+
+    return res.status(200).json(newData);
   } catch (error) {
     return res.status(500).json(error);
   }
